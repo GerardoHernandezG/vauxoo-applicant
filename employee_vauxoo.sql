39ac178b27a4f0c9d987c6d95ba19c7733034aab
Gerardo Hernández <george_odrare89@hotmail.com>
2017-03-07 15:07:03 -0600

    Add two first tables and insert data
    I created tables: employee and employee_department, added FK keys and insert data into both

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -3,11 +3,46 @@
 --       You can create database locally to test it.
 --       Consider add ';' at end sentence.
 
-CREATE TABLE employee (
+CREATE TABLE employee_department (
+    PRIMARY KEY (id),
+    id          SERIAL,
+    name        VARCHAR(50) NOT NULL,
+    description TEXT        NOT NULL
 );
 
-CREATE TABLE employee_department (
+CREATE TABLE employee (
+    PRIMARY KEY (id),
+    id          SERIAL,
+    first_name  VARCHAR(20) NOT NULL,
+    last_name   VARCHAR(20) NOT NULL,
+    department_id int REFERENCES employee_department(id) NOT NULL    
 );
+
+--INSERT DATA INTO employee_department
+  
+INSERT INTO employee_department(name, description)
+    VALUES ('Computing', 'Computing and Technical Support Department');
+INSERT INTO employee_department(name, description)
+    VALUES ('Human Resources', 'Human Resources Department');
+INSERT INTO employee_department(name, description)
+    VALUES ('Administration', 'Administration Department');
+INSERT INTO employee_department(name, description)
+    VALUES ('Marketing', 'Marketing Department');
+INSERT INTO employee_department(name, description)
+    VALUES ('Finance', 'Department of Finance');
+INSERT INTO employee_department(name, description)
+    VALUES ('Management', 'Management of the company');
+	
+--INSERT DATA INTO employee	
+
+INSERT INTO employee(first_name, last_name, department_id)
+    VALUES ('Gerardo', 'Hernandez', 1);
+INSERT INTO employee(first_name, last_name, department_id)
+    VALUES ('Jorge', 'Gonzalez', 2);
+INSERT INTO employee(first_name, last_name, department_id)
+    VALUES ('Pedro', 'Perez', 2);
+INSERT INTO employee(first_name, last_name, department_id)
+    VALUES ('Juan', 'Lopez', 6);
 
 CREATE TABLE employee_hobby (
 );
