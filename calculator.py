4d8dfcaf0234f56b85764d4cfabb723e84ac4c5e
Gerardo Hernández <george_odrare89@hotmail.com>
2017-03-07 15:55:43 -0600

    evaluation_py2_calculator
    I created a method in the class calculator that receive the size of a number list and calculates their sum

--- calculator.py
+++ calculator.py
@@ -1,16 +1,27 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
 Your module documentation here
+No modules
 """
 
 
 class CalculatorClass(object):
     """
-    Your class documentation here
+    has a method calculates the sum of a list of integer numbers
     """
+    num_list = []
 
     def sum(self, num_list):
         """
         Your method documentation here
+        method tha recevie the size of the list and calculate his numbers sum
         """
-        # your sum code here
         return "not implement yet"  # Remove this dummy line
+    suma = 0
+    entrada = int(raw_input('Ingresa el tamaño de la lista: '))
+    for num in range(entrada):
+        num_list.append(int(raw_input('Numero: ')))
+    for numero in num_list:
+        suma = suma + numero
+    print 'La suma es: ', suma
