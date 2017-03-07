1c0d1549ba7373670ab7f3f2854fd941297e0957
Gerardo Hernández <george_odrare89@hotmail.com>
2017-03-07 15:49:04 -0600

    evaluation_py1_primes
    I created a method in primes.py than receive from input an int number a check if it is prime or not

--- primes.py
+++ primes.py
@@ -1,16 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 """
-Your module documentation here
+Programa que verifica si un número es primo o no
 """
 
 
 class PrimeClass(object):
     """
     Your class documentation here
+    Verificar si un numero ingresado es o no primo
     """
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        Ingresa un número de tipo entero
+        se valida primero cuando es menor a 2
+        y de 2 en adelante obtener su modulo
+        para ver si es primo
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+        if num_int < 2:
+            return False
+        else:
+            if num_int == 2:
+                return True
+            else:
+                for num in range(2, num_int):
+                    if num_int % num == 0:
+                        return False
+                return True
+    num_int = int(raw_input('Ingresa un numero: '))
+    print is_prime('', num_int)
