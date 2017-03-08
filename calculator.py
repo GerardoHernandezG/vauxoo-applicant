658bb8a46e8fffa633c20d5a687c09005b99fd5b
Gerardo Hernández <george_odrare89@hotmail.com>
2017-03-07 20:12:25 -0600

    Remove white spaces
    Sorry I forgot to remove white spaces after having optimized the script

--- calculator.py
+++ calculator.py
@@ -9,14 +9,13 @@ No modules
 class CalculatorClass(object):
     """
     has a method calculates the sum of a list of integer numbers
-    """    
+    """
 
     def sum(self, num_list):
         """
         Your method documentation here
         method tha recevie the size of the list and calculate his numbers sum
         """
-        return "not implement yet"  # Remove this dummy line
         suma = 0
         for numero in num_list:
             suma = suma + numero
